078ae15d27c3acb9d2b163174eaedb8018d64477
Karuppiah Natarajan <karuppiah7890@gmail.com>
2021-05-09 13:13:28 +0530

    [game-of-life] add oscillators toad pattern test

--- game-of-life/engine/src/Generation.test.ts
+++ game-of-life/engine/src/Generation.test.ts
@@ -119,6 +119,42 @@ describe("Generation", () => {
           initialGeneration
         );
       });
+
+      test("Toad", () => {
+        // Period: 2
+        const cell1 = new Cell(0, 0);
+        const cell2 = new Cell(1, 0);
+        const cell3 = new Cell(2, 0);
+        const cell4 = new Cell(-1, -1);
+        const cell5 = new Cell(0, -1);
+        const cell6 = new Cell(1, -1);
+        
+        const initialGeneration = new Generation(
+          new Set([cell1, cell2, cell3, cell4, cell5, cell6])
+        );
+
+
+        const firstGenCell1 = new Cell(-1, 0);
+        const firstGenCell2 = new Cell(-1, -1);
+        const firstGenCell3 = new Cell(0, -2);
+        const firstGenCell4 = new Cell(1, 1);
+        const firstGenCell5 = new Cell(2, 0);
+        const firstGenCell6 = new Cell(2, -1);
+
+        const expectedFirstGeneration = new Generation(
+          new Set([firstGenCell1, firstGenCell2, firstGenCell3, firstGenCell4, firstGenCell5, firstGenCell6])
+        );
+
+        const firstGeneration = initialGeneration.nextGeneration();
+        expect(firstGeneration).toStrictEqual(
+          expectedFirstGeneration
+        );
+
+        const secondGeneration = firstGeneration.nextGeneration();
+        expect(secondGeneration).toStrictEqual(
+          initialGeneration
+        );
+      });
     });
   });
 });
